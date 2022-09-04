a9077b536ecf3ed581b6d76be03df45f6cce982b
Jessica Turner <jmcclung3509@gmail.com>
2022-09-04 08:29:21 -0700

    Created game function

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
+let playerSelection = "rock"
+let computerSelection= "rock"
+
 function getComputerChoice(){
    let computerChoice = randomChoice()
    
     console.log(computerChoice)
     if (computerChoice == 1){
-        console.log("Rock")
+        computerSelection = "rock"
+      
     } else if (computerChoice ==2){
-        console.log("Scissors")
+        computerSelection = "scissors"
+
     } else {
-        console.log("Paper")
+       
+        computerSelection ="paper"
+
     }
+    console.log(computerSelection)
 
 }
+getComputerChoice()
 function randomChoice(){
     let randomIndex = Math.floor((Math.random() *3) +1)
     console.log(randomIndex)
     return randomIndex
 }
-getComputerChoice()
+
+function game(){
+    if (playerSelection == "rock" && computerSelection == "rock"){
+        console.log("It's a tie")
+    } else if (playerSelection == "rock" && computerSelection == "paper"){
+        console.log("Computer Wins")
+    }else if (playerSelection =="rock" && computerSelection =="scissors"){
+        console.log("Player wins")
+    } else if (playerSelection == "paper" && computerSelection =="paper"){
+        console.log("It's a tie")
+    } else if (playerSelection == "paper" && computerSelection =="scissors") {
+        console.log("Computer Wins")
+    } else if (playerSelection == "paper" && computerSelection =="rock"){
+        console.log( "Player Wins")
+    } else if (playerSelection =="scissors" && computerSelection =="scissors"){
+        console.log ("It's a tie")
+    }else if (playerSelection =="scissors" && computerSelection == "paper"){
+        console.log("Player wins")
+    }else  {
+        console.log("Computer Wins")
+    }
+}
+game()
