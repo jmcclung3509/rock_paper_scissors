3cb9c0b749350b8111c5539982471388207aa8c4
Jessica Turner <jmcclung3509@gmail.com>
2022-09-07 09:58:47 -0700

    Styles completed, functionality completed

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-let playersInputField = document.getElementById("playerInput")
+
+//show selection per round
 let playerChoiceContainer = document.querySelector(".player_selection")
 let computerChoiceContainer = document.querySelector(".computer_selection")
+//  object image link
+let scissorImg = "fa fa-scissors"
+let paperImg = "fa fa-toilet-paper"
+let rockImg = "./images/rock_image.png"
+//object image containers to show object picked each round
+// let playerImg= document.getElementById("player_image")
+// let computerImg = document.getElementById("computer_image")
+let playerImgContainer= document.querySelector(".player_image_container")
+let computerImgContainer = document.querySelector(".computer_image_container")
+//show score after each round
 let playerScoreContainer = document.querySelector(".player_score")
 let computerScoreContainer = document.querySelector(".computer_score")
+// shows round
 let roundContainer = document.querySelector(".round_count")
-let winnerContainer = document.querySelector(".winner")
+// displays text on winner of round and winner of game
+let winnerContainer = document.querySelector(".winner_container")
+let gameWinnerContainer = document.querySelector(".game_winner_container")
+//selection of players object 
 let scissorElement = document.getElementById("scissor")
 let paperElement = document.getElementById("paper")
 let rockElement = document.getElementById("the_rock")
+//playagain buuton
 let goButton = document.getElementById("go")
-let roundWinner = document.querySelector(".round_winner")
+
 
 
 //starting values
@@ -33,7 +49,12 @@ function reset(){
     computerScore = 0
     playerScoreContainer.textContent = playerScore
     computerScoreContainer.textContent = computerScore
-    goButton.textContent = "Go!"
+    winnerContainer.innerHTML = ""
+    gameWinnerContainer.innerHTML = ""
+  if (goButton.classList.contains("show")){
+      goButton.classList.remove("show")
+      goButton.classList.add("hide")
+  }
     // game()
 }
 //get computer choice
@@ -64,93 +85,120 @@ function randomChoice(){
 
 //get player choice
 scissorElement.addEventListener("click", function (){
-    console.log("click")
-    playerSelection = "scissor"
+    if (round>=0 && round < 5){
+    playerImgContainer.innerHTML = ""
+    computerImgContainer.innerHTML = ""
+    playerSelection = "scissors"
     computerSelection = getComputerChoice()
    playRound(playerSelection, computerSelection)
-})
+} else {
+
+}})
 paperElement.addEventListener("click", function (){
-    console.log("click")
+    if (round>=0 && round < 5){
+    playerImgContainer.innerHTML = ""
+    computerImgContainer.innerHTML = ""
     playerSelection = "paper"
     computerSelection = getComputerChoice()
    playRound(playerSelection, computerSelection)
-})
+} else {
+
+}})
 rockElement.addEventListener("click", function (){
+    if (round>=0 && round < 5){
+    playerImgContainer.innerHTML = ""
+    computerImgContainer.innerHTML = ""
     console.log("click")
     playerSelection = "rock"
+    
+    playerSelectionImg = ""
     computerSelection = getComputerChoice()
    playRound(playerSelection, computerSelection)
-})
+} else {
+
+}})
 
 
 
 function playRound(playerSelection, computerSelection){
     console.log(round)
-if (round < 5){
+if (round <= 5){
 
     round ++
-
- console.log(playerSelection, computerSelection)
  
- 
- computerChoiceContainer.textContent = computerSelection
- playerChoiceContainer.textContent=playerSelection
+//  computerChoiceContainer.textContent = computerSelection
+//  playerChoiceContainer.textContent=playerSelection
  roundContainer.innerText = round
 
-    
-   
-    console.log(round)
-    if (playerSelection == "") {
-        alert ("Please pick an object")
-    
 
- }else if (playerSelection == "rock" && computerSelection == "rock"){
+ if (playerSelection == "rock" && computerSelection == "rock"){
         computerScore++ && playerScore++
         getScore()
-        winnerContainer.textContent = "It's a tie"
-      
-   
+        winnerContainer.innerHTML = '<p class ="winner_text"><span>It is a tie</span></p>'  
+        getPlayerImage(playerSelection)
+      getComputerImage(computerSelection)
 
     } else if (playerSelection == "rock" && computerSelection == "paper"){
         computerScore++
         getScore()
-        winnerContainer.textContent = "Computer wins this round!  Paper beats Rock!"
-
+        winnerContainer.innerHTML = '<p class = "winner_text">Computer wins this round! </br> <span> Paper beats Rock!</span></p>'
+        getPlayerImage(playerSelection)
+        getComputerImage(computerSelection)
 
     }else if (playerSelection =="rock" && computerSelection =="scissors"){
-    
         playerScore ++
         getScore()
-        winnerContainer.textContent =  "Player wins this round! Rock beats Scissors"
+        winnerContainer.innerHTML=  '<p class = "winner_text"> Player wins this round! </br> <span> Rock beats Scissors</span></p>' 
+        getPlayerImage(playerSelection)
+        getComputerImage(computerSelection)
+
     } else if (playerSelection == "paper" && computerSelection =="paper"){
         computerScore++ && playerScore++
         getScore()
-        winnerContainer.textContent = "It'a a tie!"
+        winnerContainer.innerHTML = '<p class ="winner_text"><span>It is a tie</span></p>'
+        getPlayerImage(playerSelection)
+      getComputerImage(computerSelection)
+
     } else if (playerSelection == "paper" && computerSelection =="scissors") {
         computerScore++
         getScore()
-        winnerContainer.textContent = "Computer wins this round! Scissors beat Paper!"
+        winnerContainer.innerHTML = '<p class = "winner_text"> Computer wins this round! </br> <span>Scissors beat Paper!</span></p>'
+        getPlayerImage(playerSelection)
+        getComputerImage(computerSelection)
+
     } else if (playerSelection == "paper" && computerSelection =="rock"){
         playerScore ++
      getScore()
-        return "Player wins this round! Paper beats Rock!"
+       winnerContainer.innerHTML = '<p class = "winner_text">Player wins this round! </br> <span>Paper beats Rock!</span></p>'
+       getPlayerImage(playerSelection)
+       getComputerImage(computerSelection)
+
     } else if (playerSelection =="scissors" && computerSelection =="scissors"){
         computerScore++ && playerScore++
         getScore()
-        winnerContainer.textContent =  "It's a tie"
+        winnerContainer.innerHTML = '<p class ="winner_text"><span>It is a tie</span></p>'
+        getPlayerImage(playerSelection)
+        getComputerImage(computerSelection)
+
     }else if (playerSelection =="scissors" && computerSelection == "paper"){
         playerScore ++
         getScore()
-        winnerContainer.textContent = "Player wins this round! Scissors beats Paper!"
+        winnerContainer.innerHTML = '<p class = "winner_text"> Player wins this round!</br> <span> Scissors beats Paper!</span></p>'
+        getPlayerImage(playerSelection)
+        getComputerImage(computerSelection)
     }else  {
       computerScore++
       getScore()
-      winnerContainer.textContent = "Computer wins this round! Rock beats Scissors!"
+      winnerContainer.innerHTML = '<p class = "winner_text">Computer wins this round!</br>  <span> Rock beats Scissors!</span></p>'
+      getPlayerImage(playerSelection)
+      getComputerImage(computerSelection)
     }
 
     
 } else if (round = 5){
     reset()
+
+   
 } 
 }
 
@@ -159,40 +207,71 @@ if (round < 5){
 
 function game() {
   for (let i=0; i< 5; i++){
-if (i <5){
-
+if (i <5 || i == 0){
     roundContainer.textContent = `Round ${round}`
-
 } else {
+    winnerContainer.textContent = ""
     getScore()
 }
 }
 }
+function getPlayerImage(playerSelection) {
+    console.log(playerSelection)
+    if (playerSelection == "scissors") {
+        playerImgContainer.innerHTML = '<img class = "scissors_gif" src = "./images/scissors.gif" alt = "scissors gif" />'   
+    } else if (playerSelection =="paper"){
+        playerImgContainer.innerHTML = '<img class = "paper_gif" src = "./images/1391-paper-waste-lineal.gif" alt = "paper GIF">'
+    } else if (playerSelection =="rock") {
+        playerImgContainer.innerHTML = '<iframe src="https://giphy.com/embed/uXQIwlDNzytyg" style="frameBorder="0" class="rock-gif"></iframe>'
+    }
+}
+function getComputerImage(computerSelection){
+        if (computerSelection == "scissors") {
+            computerImgContainer.innerHTML = '<img class = "scissors_gif" src = "./images/scissors.gif" alt = "scissors gif" />'    
+        } else if (computerSelection =="paper"){
+            computerImgContainer.innerHTML = '<img class = "paper_gif" src = "./images/1391-paper-waste-lineal.gif" alt = "paper GIF">'
+        } else if (computerSelection == "rock"){
+            computerImgContainer.innerHTML = '<iframe src="https://giphy.com/embed/uXQIwlDNzytyg" style="frameBorder="0" class="rock-gif"></iframe>'
+        }
+    }
+
 function getScore(){
-    // playerSelection = ""
 
-    if (round <= 5){
-
+    if (round <5){
         computerScoreContainer.textContent = computerScore
         playerScoreContainer.textContent = playerScore
        game()
-    } else if (round > 5) {
-        console.log("round 5")
-        roundContainer.textContent = "Game Over"
-        goButton.textContent = "Play Again"
-        if (playerScore > computerScore){
-          
-            winnerContainer.textContent = `PLAYER WINS GAME! ${playerScore} vs ${computerScore}`
-        } else if (computerScore > playerScore){
-            console.log ("Computer Wins")
-            winnerContainer.innerText = `COMPUTER WINS GAME! ${computerScore} vs. ${playerScore}`
-
-        } else {
-            console.log("Tie")
-            winnerContainer.textContent = "TIE GAME!"
+    } else if (round  = 5) {
+        computerScoreContainer.textContent = computerScore
+        playerScoreContainer.textContent = playerScore
+        winnerContainer.textContent = ""
+        gameOver(playerScore, computerScore)
+        if (goButton.classList.contains("hide")){
+            goButton.classList.remove("hide")
+            goButton.classList.add("show")
         }
+   
+  
+       
 
     }
 }
+function gameOver(playerScore, computerScore) {
+    roundContainer.textContent = "Game Over"
 
+    if (Number(playerScore) > Number(computerScore)){
+        console.log("player wins")
+        gameWinnerContainer.innerHTML = `<h2> PLAYER WINS GAME </h2> `
+        // winnerContainer.innerHTML = `<h3>PLAYER WINS GAME! </h3> </br> <h5>${playerScore} vs ${computerScore}</h5>`
+    } else if (Number(computerScore) > Number(playerScore)){
+
+        console.log ("Computer Wins")
+        gameWinnerContainer.innerHTML = `<h2> COMPUTER WINS GAME</h2> `
+        // winnerContainer.innerHTML= `<h3>COMPUTER WINS GAME! </h3> </br> <h5>${computerScore} vs. ${playerScore}</h5>`
+
+    } else {
+        console.log("Tie")
+        gameWinnerContainer.innerHTML = "<h2>TIE GAME!</h2>"
+    }
+}
 
